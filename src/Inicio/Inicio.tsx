2a661ce727b6dd0c831af0f0fd8b32e15a92a100
Kaike Barros <kaikebarros32@gmail.com>
2026-08-05 17:19:04 -0300

    feat: adiciona filtro de produtos por busca categoria e melhorias no css

---
+++ src/Inicio/Inicio.tsx
@@ -0,0 +1,176 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { FiPlus, FiSearch } from "react-icons/fi";
+import { LuCakeSlice, LuCupSoda, LuPackage, LuPizza } from "react-icons/lu";
+import pizzaCardUm from "../assets/pizza-card-1.png";
+import { db } from "../Layout/Services/Firebase";
+import "./Inicio.css";
+function Inicio() {
+  interface Produto {
+    id: string;
+    nome: string;
+    preco: number;
+    categoria: string;
+    imagem: string;
+    descricao: string;
+    resumo: string;
+  }
+
+  const [produtos, setProdutos] = useState<Produto[]>([]);
+  const [descricaoAberta, setDescricaoAberta] = useState<string | null>(null);
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState("todos");
+  const [textoInput, setTextoInput] = useState("");
+  async function buscarProdutos(): Promise<void> {
+    const produtosRef = collection(db, "produtos");
+
+    const resposta = await getDocs(produtosRef);
+
+    const lista: Produto[] = resposta.docs.map((doc) => ({
+      id: doc.id,
+      ...(doc.data() as Omit<Produto, "id">),
+    }));
+
+    setProdutos(lista);
+  }
+
+  useEffect(() => {
+    buscarProdutos();
+  }, []);
+
+  return (
+    <>
+      <div className="inicio">
+        <div className="input">
+          <label htmlFor="buscar" className="buscar"></label>
+
+          <input
+            type="search"
+            id="buscar"
+            placeholder="O que você vai pedir hoje?"
+            onChange={(evento)=>{
+              setTextoInput(evento.target.value)
+            }}
+          />
+
+          <FiSearch />
+        </div>
+        <div className="card">
+          <img src={pizzaCardUm} alt="" />
+
+          <div className="conteudo-card">
+            <h2>QUENTE E PRONTO</h2>
+
+            <h1>PIZZA</h1>
+
+            <p>Large Pepperoni</p>
+            <button className="btn-cta">Peça agora →</button>
+          </div>
+        </div>
+        <section>
+          <div className="categorias">
+            <div
+              className="tradicionais"
+              onClick={() => {
+                {
+                  setCategoriaSelecionada(categoriaSelecionada ==="tradicionais"?"todos": "tradicionais" );
+                }
+              }}
+            >
+              <LuPizza />
+              <span>Tradicionais</span>
+            </div>
+            <div
+              className="combos"
+              onClick={() => {
+                {
+                  setCategoriaSelecionada(categoriaSelecionada ==="combos"? "todos":"combos");
+                }
+              }}
+            >
+              <LuPackage />
+              Combos
+            </div>
+            <div
+              className="bebidas"
+              onClick={() => {
+                {
+                  setCategoriaSelecionada(categoriaSelecionada ==="bebidas"? "todos":"bebidas");
+                }
+              }}
+            >
+              <LuCupSoda />
+              <span>Bebidas</span>
+            </div>
+            <div
+              className="sobremesa"
+              onClick={() => {
+                {
+                  setCategoriaSelecionada(categoriaSelecionada ==="sobremesa"?"todos":"sobremesa");
+                }
+              }}
+            >
+              <LuCakeSlice />
+              <span>Sobremesa</span>
+            </div>
+          </div>
+        </section>
+
+        <h2 className="subtitulo">Pra você</h2>
+
+        <section>
+          <div className="produtos">
+            {}
+
+            {produtos
+              .filter((produto) => categoriaSelecionada === "todos" ? true :produto.categoria ===categoriaSelecionada
+            
+            
+            
+            
+            )
+
+
+            .filter((produto)=> produto.nome.toLocaleLowerCase().includes( textoInput.toLocaleLowerCase()) 
+
+
+            )
+
+         
+              .map((produto) => (
+                <div className="card-produto" key={produto.id}>
+                  <img src={pizzaCardUm} alt="" />
+
+                  <div className="conteudo-produto">
+                    <h2>{produto.nome}</h2>
+
+                    <p
+                      className={`resumo ${descricaoAberta === produto.id ? " aberta" : ""}`}
+                      onClick={() =>
+                        setDescricaoAberta(
+                          descricaoAberta === produto.id ? null : produto.id,
+                        )
+                      }
+                    >
+                      {produto.descricao}
+                    </p>
+                    <p className="preco">
+                      {produto.preco.toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                      })}
+                    </p>
+                  </div>
+                  <button className="btn-add">
+                    {" "}
+                    <FiPlus />
+                  </button>
+                </div>
+              ))}
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default Inicio;
